c6a8cba4cfe1a3fd602a6ccbe6cec3a7edac58d2
riku3840 <bb35319047@ms.nagasaki-u.ac.jp>
2020-07-16 18:40:36 +0900

    0716

--- k02/k02.c
+++ k02/k02.c
@@ -7,11 +7,15 @@ extern double p_stdnorm(double z);
 
 int main(void)
 {
-    double val;
+    double val,sigma_a,sigma_b,u_a,u_b,z_a,z_b,x_a,x_b;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
-    double L1=1,L2=1;
+    double L_a=1,L_b=1;
+    u_a=170.8;
+    u_b=169.7;
+    sigma_a=5.43;
+    sigma_b=5.5;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -26,7 +30,12 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
+        z_a=(val-u_a)/sigma_a;
+        z_b=(val-u_b)/sigma_b;
+        x_a=p_stdnorm(z_a); 
+        x_b=p_stdnorm(z_b);
+        L_a=L_a*x_a;
+        L_b=L_b*x_b;
 
     
 
@@ -39,8 +48,8 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    printf("L_A: %f\n",max_val);
-    printf("L_B: %f\n",min_val);
+    printf("L_A: %f\n",L_a);
+    printf("L_B: %f\n",L_b);
 
     return 0;
 
