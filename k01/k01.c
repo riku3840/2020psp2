85c351edd37169a67482e14eda95c5501c922278
riku3840 <bb35319047@ms.nagasaki-u.ac.jp>
2020-07-16 17:52:29 +0900

    0715

--- k01/k01.c
+++ k01/k01.c
@@ -40,9 +40,9 @@ int main(void)
     {
         sscanf(buf,"%lf",&val);
         a++;
+        var=var_online(ave,square_ave,a);
         square_ave=ave_online(square_ave,pow(val,2),a);
         ave=ave_online(ave,val,a);
-        var=var_online(ave,square_ave,a);
     }
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
